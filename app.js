02431149c3ae52a5bd5e2c85bd79c2cebb4c5523
Chris Tomlinson <crtcheesehead@users.noreply.github.com>
2023-04-29 20:56:08 -0400

    final submission

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Let's get this party started!");
+const $gifContainer = $('#gifContainer');
+const $searchInput = $("#search");
+
+function addGif(res){
+    let numResults = res.data.length;
+    if (numResults) {
+        let randomIdx = Math.floor(Math.random() *numResults);
+        let randomUrl = res.data[randomIdx].url;
+        let $newCol = $("<div>", {class: "gif"});
+        let $newGif = $("<img>", {
+            src: randomUrl
+        });
+            
+        $newCol.append($newGif);
+        $gifContainer.append($newCol);
+    }
+
+}
+
+$('form').on("submit", async function(evt){
+    evt.preventDefault();
+
+    let searchTerm = $searchInput.val();
+    $searchInput.val("");
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+            q: searchTerm,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+    addGif(response.data);
+})
+
+$('#remove').on("click", function(){
+    $gifContainer.empty();
+})
